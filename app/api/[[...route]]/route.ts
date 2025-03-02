02119245151ec20a0ae0ab405aee25580f26f411
chanvichea169 <chanvicha169@gmail.com>
2025-03-02 22:47:06 +0700

    setting is deleted

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,7 +4,6 @@ import summary from "./summary";
 import accounts from "./accounts";
 import categories from "./categories";
 import transactions from "./transactions";
-import setting from "./setting";
 
 export const runtime = "edge";
 
@@ -14,8 +13,7 @@ const routes = app
   .route("/summary", summary)
   .route("/accounts", accounts)
   .route("/categories", categories)
-  .route("/transactions", transactions)
-  .route("/setting", setting);
+  .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
