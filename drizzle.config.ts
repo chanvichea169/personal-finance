4791928753b1d41299a0e3dd5a8c0e12388480ef
chanvichea169 <chanvicha169@gmail.com>
2025-03-06 23:39:46 +0700

    new update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 
 const { defineConfig } = require("drizzle-kit");
+import { accounts } from "./db/schema";
 
 config({ path: ".env.local" });
 
@@ -9,7 +10,7 @@ export default defineConfig({
   driver: "pg",
   dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
