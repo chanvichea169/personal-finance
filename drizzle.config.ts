f4088de051b93f47efdbe974ba9eb6105cc4ef04
chanvichea169 <chanvicha169@gmail.com>
2025-03-06 22:48:48 +0700

    Update tsconfig.json and header.tsx with font style changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,6 @@
 import { config } from "dotenv";
 
 const { defineConfig } = require("drizzle-kit");
-import { accounts } from "./db/schema";
 
 config({ path: ".env.local" });
 
@@ -10,7 +9,7 @@ export default defineConfig({
   driver: "pg",
   dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
