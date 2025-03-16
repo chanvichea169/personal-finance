d9ce7519018cf312beaa439273397f023d0ac91a
chanvichea169 <chanvicha169@gmail.com>
2025-03-16 11:20:59 +0700

    Updated deprecated dependencies

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -3,5 +3,9 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-export const sql = neon(process.env.DATABASE_URL!);
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
+export const sql = neon(process.env.DATABASE_URL);
 export const db = drizzle(sql, { schema });
